81b9a317df0c4fc1eb75c868389b373ff572199e
arcum-omni <travis.l.eiler@gmail.com>
2020-02-22 15:23:26 -0800

    Created Class Course.cs

---
+++ NestedObjectsRedo/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjectsRedo
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+
+        public string CourseTitle { get; set; }
+
+        public string CourseDescription { get; set; }
+
+        public byte CreditHours { get; set; }
+    }
+}
